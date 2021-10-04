90185e55f5db252042dd32ee573e3ca1e9f2f068
Sergio N <serch.np@gmail.com>
2021-10-03 22:14:29 -0500

    Definición de las rutas para las apis, ademas de la función que llena la base de datos

--- pipeline/urls.py
+++ pipeline/urls.py
@@ -4,22 +4,15 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	# url(r'^carrito/$', 						views.showShoppingCart, 		name='show-cart'),
-	# url(r'^carrito/limpiar/$', 				views.emptyShoppingCart, 		name='clean-cart'),
-	# url(r'^carrito/concretar-compra/$', 	views.checkout, 				name='checkout'),
-	# url(r'^carrito/procesar/$', 			apiviews.process_cart, 			name='process-purchase'),
-	# url(r'^carrito/comprar-ahora/$', 		apiviews.add_secundary_cart, 	name='add-secundario-cart-api'),
-	# url(r'^carrito/agregar/$', 				apiviews.add_cart, 				name='add-cart-api'),
-	# url(r'^carrito/aumentar/$', 			apiviews.increase_cart, 		name='increase-cart-api'),
-	# url(r'^carrito/disminuir/$', 			apiviews.decrease_cart, 		name='decrease-cart-api'),
-	# url(r'^carrito/cantidad/$', 			apiviews.quantity_cart, 		name='quantity-cart-api'),
-	# url(r'^carrito/quitar/$', 				apiviews.remove_cart, 			name='remove-cart-api'),
-
 	# URL para leer los datos del metrobus
-	path('pipeline/metrobus/consultar', 	views.getMetrobusInfo, 					name='metrobus-info'),
-	# URL empleada al momento de procesar la compra para envíos foráneos
-	# path('carrito/cotizar-envio/', 			apiviews.cotizar_envio, 		name='quote-shipping'),
-	# # path('carrito/rastrear-envio/', 		apiviews.rastrear_envio, 		name='track-shipping'),
-
-	# path('carrito/olvidado/', 				apiviews.forgetCart, 			name='forget-cart'),	
+	path('pipeline/metrobus/consultar', 			views.getMetrobusInfo, 					name='metrobus-info'),
+	
+	# URL API Obtener una lista de unidades disponibles
+	path('api/units_available',						views.getUnitsAvailable,				name='units-available'),
+	# URL API Consultar los el historial de ubicaciones/fechas de una unidad dado su ID
+	path('api/unit_records/<int:unit_id>', 			views.getUnitRecords,					name='unit-records'),
+	# URL API Obtener una lista de alcaldías disponibles
+	path('api/mayoralties_available',				views.getMayoraltiesAvailable,			name='mayoralties-available'),
+	# URL API Obtener una lista de unidades que hayan estado dentro de una alcaldía
+	path('api/mayoralty_units/<int:mayoralty_id>', 	views.getUnitsFromMayoralty,			name='units-from-mayoralty'),
 ]
