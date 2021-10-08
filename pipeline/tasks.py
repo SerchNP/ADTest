020e3a700165d34cf579c668e4ee937966f1e536
Sergio N <serch.np@gmail.com>
2021-10-08 03:16:50 -0500

    Salida a consola

--- pipeline/tasks.py
+++ pipeline/tasks.py
@@ -1,4 +1,4 @@
-from celery import Celery, shared_task
+from celery import shared_task
 from celery.utils.log import get_task_logger
 
 from .views import getMetrobusInfo
@@ -9,6 +9,7 @@ logger = get_task_logger(__name__)
 # Se define la función de la tarea que se ejecutará periodicamente
 @shared_task(bind=True)
 def get_metrobus_info_task(self):
+	print('Leyendo datos del metrobus')
 	getMetrobusInfo(self.request)
 	logger.info('Datos obtenidos')
 	return "Done"
